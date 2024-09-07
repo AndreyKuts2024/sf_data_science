2c3ccffce03657db1f23bb7ece9e0dcd0593ee58
AndreyKuts2024 <espritdecorps@mail.ru>
2024-09-07 22:53:44 +0300

    Initial commit

---
+++ Task 8.1 Module 8/game_v3.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def score_game(game_core_v3) -> int:
+    """Функция для оценки, за какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        game_core_v3 ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(game_core_v3(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+    
+
+def game_core_v3(number: int = 1) -> int:
+    """Функция которая угадывает загаданное число учитывая информацию больше или меньше случайное число нужного нам числа
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+    count = 0
+    predict = np.random.randint(1, 101)
+    
+    min_zone = 1 # Определяем минимальный диапазон чисел для очередной попытки угадывания, на старте 1
+    max_zone = 101 # Определяем минимальный диапазон чисел для очередной попытки угадывания, на старте 101
+    
+    while number != predict:
+        count += 1
+        
+        if number > predict:
+            min_zone = predict # корректируем меньшую границу диапазона
+            predict = np.random.randint(min_zone, max_zone) # Берем случайное число в актуальном диапазоне
+        elif number < predict:
+            max_zone = predict # корректируем большую границу диапазона
+            predict = np.random.randint(min_zone, max_zone) # Берем случайное число в актуальном диапазоне
+    return count
+    # Ваш код заканчивается здесь
+
+    return count
+
+
+print('Run benchmarking for game_core_v3: ', end='')
+score_game(game_core_v3)

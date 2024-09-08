7ff5c489dc0da0ff36d431d1dca1f5e1f40b7955
AndreyKuts2024 <espritdecorps@mail.ru>
2024-09-08 14:47:56 +0300

    update

--- Project0/game_v2.py
+++ Project0/game_v2.py
@@ -34,18 +34,17 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
     return score
 
 
 if __name__ == "__main__":
     # RUN
     score_game(random_predict)
-
